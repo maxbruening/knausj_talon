a66e00a011f983ec90e39b00d4b336531dc1dd1b
max.bruening <56445556+maxbruening@users.noreply.github.com>
2024-03-08 12:30:41 +0100

    implemented imperative.py

--- lang/stata/stata.py
+++ lang/stata/stata.py
@@ -17,6 +17,42 @@ class UserActions:
         actions.insert("}")
         actions.key("up:2 right:2")
 
+    def code_state_else_if():
+        actions.insert("else if  {")
+        actions.key("enter tab enter left")
+        actions.insert("}")
+        actions.key("up:2 right:7")
+
+    def code_state_else():
+        actions.insert("else  {")
+        actions.key("enter tab enter left")
+        actions.insert("}")
+        actions.key("up:2 right:4")  
+
+    def code_state_do():
+        actions.insert("do {")
+        actions.key("enter tab enter left")
+        actions.insert("} while ()")
+        actions.key("up tab")
+
+    def code_state_for():    
+        actions.insert("for () {")
+        actions.key("enter tab enter left")
+        actions.insert("}")
+        actions.key("up:2 right:4")
+
+    def code_state_for_each():
+        actions.insert("foreach  in {")
+        actions.key("enter tab enter left")
+        actions.insert("}")
+        actions.key("up:2 right:7")
+
+    def code_break():
+        actions.insert("break")
+
+    def code_next():
+        actions.insert("continue")        
+
     def code_comment_line_prefix():
        actions.auto_insert("* ") 
        # actions.auto_insert("// ") 
