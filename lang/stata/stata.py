a01f36dc5da84ab4ffff09b00928a7591a687058
max.bruening <56445556+maxbruening@users.noreply.github.com>
2024-03-08 10:04:19 +0100

    more stata snippets

--- lang/stata/stata.py
+++ lang/stata/stata.py
@@ -5,3 +5,26 @@ ctx = Context()
 ctx.matches = r"""
 code.language: stata
 """
+
+@ctx.action_class("user")
+class UserActions:
+    def code_block():
+        actions.auto_insert("\n")
+
+    def code_state_if():
+        actions.insert("if  {")
+        actions.key("enter tab enter left")
+        actions.insert("}")
+        actions.key("up:2 right:2")
+
+    def code_comment_line_prefix():
+       actions.auto_insert("* ") 
+       # actions.auto_insert("// ") 
+
+
+
+    
+
+
+
+
--- lang/stata/stata.talon
+++ lang/stata/stata.talon
@@ -1,3 +1,16 @@
 code.language: stata
 -
+tag(): user.code_imperative
+
+tag(): user.code_comment_block_c_like
+tag(): user.code_comment_block
+tag(): user.code_comment_line
+
+
+
+
+
+
+
+
 regress: insert("reg")
