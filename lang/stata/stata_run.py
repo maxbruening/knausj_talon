c2997b86e9fee1b3d799a41ce2744d48f56ee2b5
max.bruening <56445556+maxbruening@users.noreply.github.com>
2024-03-08 15:25:48 +0100

    draft stata_run

---
+++ lang/stata/stata_run.py
@@ -0,0 +1,23 @@
+# from talon import Context, actions, settings
+
+# ctx = Context()
+
+# ctx.matches = r"""
+# code.language: stata
+# """
+
+# @ctx.action_class("user")
+# class UserActions:
+#    def stata_run_do_editor():
+#       actions.user.switcher_focus("Stata")
+#       actions.sleep("500ms")
+#       # actions.key(ctrl-1)
+#       # actions.sleep("500ms")
+#       # actions.insert("doedit")
+#       # actions.key("enter")
+#       # actions.sleep("500ms")
+#       # actions.edit.paste()
+#       # actions.key("ctrl-d")
+#       # user.delete_all()
+#       # actions.app.window_close()
+   
--- lang/stata/stata_run.talon
+++ lang/stata/stata_run.talon
@@ -1,6 +1,28 @@
 code.language: stata
 -
-Oppenheimer: 
+settings():
+   key_wait = 4
+   insert_wait = 7
+
+
+due this: 
    edit.copy()
+   # user.stata_run_do_editor()
+   sleep(500ms)
    user.switcher_focus("Stata")
-   #insert word doedit     
+   sleep(500ms)
+   key(ctrl-1)
+   sleep(500ms)
+   insert("doedit")
+   key(enter)
+   sleep(500ms)
+   edit.paste()
+   edit.select_line()
+   sleep(500ms)
+   key(ctrl-d)
+   sleep(500ms)
+   user.delete_all()
+   app.window_close()
+   sleep(500ms)
+   edit.right()
+   key(enter)
