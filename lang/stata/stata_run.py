001567fa3f7de6ce33590c3f0a8684c7b9fe56ae
max.bruening <56445556+maxbruening@users.noreply.github.com>
2024-03-11 12:49:24 +0100

    next draft stata_run

--- lang/stata/stata_run.py
+++ lang/stata/stata_run.py
@@ -1,23 +1,54 @@
-# from talon import Context, actions, settings
+from talon import Module, Context, actions, settings
 
-# ctx = Context()
+mod = Module()
 
-# ctx.matches = r"""
-# code.language: stata
-# """
+@mod.action_class
+class Actions:
+    def stata_run_do_editor(): "Run stata code in the stata application via the do-file editor."
 
-# @ctx.action_class("user")
-# class UserActions:
-#    def stata_run_do_editor():
-#       actions.user.switcher_focus("Stata")
-#       actions.sleep("500ms")
-#       # actions.key(ctrl-1)
-#       # actions.sleep("500ms")
-#       # actions.insert("doedit")
-#       # actions.key("enter")
-#       # actions.sleep("500ms")
-#       # actions.edit.paste()
-#       # actions.key("ctrl-d")
-#       # user.delete_all()
-#       # actions.app.window_close()
+ctx = Context()
+
+ctx.matches = r"""
+code.language: stata
+"""
+
+@ctx.action_class("user")
+class UserActions:
+   def stata_run_do_editor():
+      actions.user.switcher_focus("Stata")
+      actions.sleep("500ms")
+      # key(ctrl-1)
+      # sleep(500ms)
+      # insert("doedit")
+      # key(enter)
+      # sleep(500ms)
+      # edit.paste()
+      # edit.select_line()
+      # sleep(500ms)
+      # key(ctrl-d)
+      # sleep(500ms)
+      # user.delete_all()
+      # app.window_close()
+      # sleep(500ms)
+      # edit.right()
+      # key(enter)
+
+
+
+
+
+
+
+
+
+
+      # actions.key(ctrl-1)
+      # actions.sleep("500ms")
+      # actions.insert("doedit")
+      # actions.key("enter")
+      # actions.sleep("500ms")
+      # actions.edit.paste()
+      # actions.key("ctrl-d")
+      # user.delete_all()
+      # actions.app.window_close()
    
--- lang/stata/stata_run.talon
+++ lang/stata/stata_run.talon
@@ -5,24 +5,8 @@ settings():
    insert_wait = 7
 
 
-due this: 
+do this: 
    edit.copy()
-   # user.stata_run_do_editor()
-   sleep(500ms)
-   user.switcher_focus("Stata")
-   sleep(500ms)
-   key(ctrl-1)
-   sleep(500ms)
-   insert("doedit")
-   key(enter)
-   sleep(500ms)
-   edit.paste()
-   edit.select_line()
-   sleep(500ms)
-   key(ctrl-d)
-   sleep(500ms)
-   user.delete_all()
-   app.window_close()
-   sleep(500ms)
-   edit.right()
-   key(enter)
+   user.stata_run_do_editor()
+
+   
