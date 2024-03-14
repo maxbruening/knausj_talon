beda9cdc1835a9b39da5d161e474abdd5efc78bc
max.bruening <56445556+maxbruening@users.noreply.github.com>
2024-03-14 18:06:05 +0100

    working version of stata_run

--- lang/stata/stata_run.py
+++ lang/stata/stata_run.py
@@ -1,10 +1,13 @@
-from talon import Module, Context, actions
+from talon import Module, Context, actions, ui
 
 mod = Module()
 
 
 @mod.action_class
 class Actions:
+    def focus_stata_instance():
+        "focus stata instance"
+
     def stata_run_do_editor():
         "Run selected text in an open version of the Stata application via its do-file editor."
 
@@ -20,8 +23,15 @@ not win.title: Do-file Editor
 
 @ctx.action_class("user")
 class UserActions:
+    def focus_stata_instance():
+        active_windows = ui.windows()
+        for w in active_windows:
+            if w.title.startswith("Stata/"):
+                w.focus()
+                break
+
     def stata_run_do_editor():
-        actions.user.switcher_focus("Stata")
+        actions.user.focus_stata_instance()
         actions.sleep("300ms")
         actions.key("ctrl-1")
         actions.sleep("100ms")
