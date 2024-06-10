6e6938448c1ad2c8a3d119f5beac6277abbfb4c4
max.bruening <56445556+maxbruening@users.noreply.github.com>
2024-06-10 16:20:38 +0200

    Second solution. Also does not work with comments

--- apps/microsoft_word/microsoft_word_win.py
+++ apps/microsoft_word/microsoft_word_win.py
@@ -42,29 +42,51 @@ class Actions:
 @ctx.action_class("user")
 class UserActions:
     def dictation_peek(left: bool, right: bool) -> tuple[Optional[str], Optional[str]]:
-
+        """hello"""
         if not (left or right):
             return None, None
         before, after = None, None
-        
-        # go right first
-        actions.edit.extend_word_right()
-        actions.edit.extend_word_right()
-        after = actions.edit.selected_text()
-        actions.edit.left() #deselect
-        
-        # go left
+        actions.insert(" ")
         if left:
-            actions.edit.extend_line_start()
-            before = actions.edit.selected_text()
-            if before != "":
-                actions.edit.right()  # deselect
-                # if there is a newline to the right and the line was not empty, we need to go left again
-                if after.startswith("\r\n"):
-                    actions.edit.left()
-        
-        print('after.'+after+".end")
-        print("before."+str(before)+".end")
-        if after.startswith("\r\n"):
-            print("If there is a newline, I detect it.")
+            actions.edit.extend_word_left()
+            actions.edit.extend_word_left()
+            before = actions.edit.selected_text()[:-1]
+            actions.key("shift-f5")  
+        if not right:
+            actions.key("backspace")  # remove the space
+        else:
+            actions.edit.left()  # go left before space
+            actions.edit.extend_word_right()
+            actions.edit.extend_word_right()
+            after = actions.edit.selected_text()[1:]
+            actions.edit.left()
+            actions.key("delete")  # remove space
         return before, after
+
+    # def dictation_peek(left: bool, right: bool) -> tuple[Optional[str], Optional[str]]:
+
+    #     if not (left or right):
+    #         return None, None
+    #     before, after = None, None
+        
+    #     # go right first
+    #     actions.edit.extend_word_right()
+    #     actions.edit.extend_word_right()
+    #     after = actions.edit.selected_text()
+    #     actions.edit.left() #deselect
+        
+    #     # go left
+    #     if left:
+    #         actions.edit.extend_line_start()
+    #         before = actions.edit.selected_text()
+    #         if before != "":
+    #             actions.edit.right()  # deselect
+    #             # if there is a newline to the right and the line was not empty, we need to go left again
+    #             if after.startswith("\r\n"):
+    #                 actions.edit.left()
+        
+    #     print('after.'+after+".end")
+    #     print("before."+str(before)+".end")
+    #     if after.startswith("\r\n"):
+    #         print("If there is a newline, I detect it.")
+    #     return before, after
