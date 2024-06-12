41def798dc0b121d9b86038ac2769d6b59845caa
max.bruening <56445556+maxbruening@users.noreply.github.com>
2024-06-12 16:02:36 +0200

    implement code_run tag for stata's do-file editor

---
+++ apps/stata/stata_do_file_editor.py
@@ -0,0 +1,41 @@
+from talon import Context, actions
+ctx = Context()
+
+ctx.matches = r"""
+os: windows
+app: stata
+code.language: stata
+win.title: /^Do-file Editor/
+"""
+
+@ctx.action_class("user")
+class UserActions:
+    def code_run_selection():
+        actions.key("ctrl-d")
+
+    def code_run_line_start():
+        actions.edit.line_start()
+        actions.key("ctrl-d")
+
+    def code_run_line_end():
+        actions.edit.line_start()
+        actions.key("ctrl-d")
+
+    def code_run_line():
+        actions.edit.select_line()
+        actions.key("ctrl-d")
+
+    def code_run_file():
+        actions.edit.select_all()
+        actions.edit.copy()
+        actions.key("ctrl-d")
+
+    def code_run_file_start():
+        actions.edit.extend_file_start()
+        actions.edit.copy()
+        actions.key("ctrl-d")
+
+    def code_run_file_end():
+        actions.edit.extend_file_end()
+        actions.edit.copy()
+        actions.key("ctrl-d")
--- apps/stata/stata_do_file_editor.talon
+++ apps/stata/stata_do_file_editor.talon
@@ -1,25 +1,7 @@
 # Commands for the Stata Do-File Editor
 os: windows
 app: stata
+code.language: stata
 win.title: /^Do-file Editor/
 -
-do this: key(ctrl-d)
-
-do line:
-    edit.select_line()
-    key(ctrl-d)
-
-do (all | file):
-    edit.select_all()
-    edit.copy()
-    key(ctrl-d)
-
-do way up:
-    edit.extend_file_start()
-    edit.copy()
-    key(ctrl-d)
-
-do way down:
-    edit.extend_file_end()
-    edit.copy()
-    key(ctrl-d)
+tag(): user.code_run
