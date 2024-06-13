3beb69e894be28bf18194a083b8d0e358c541efc
max.bruening <56445556+maxbruening@users.noreply.github.com>
2024-06-13 15:50:02 +0200

    break overly long line

--- apps/stata/stata_do_file_editor.py
+++ apps/stata/stata_do_file_editor.py
@@ -1,4 +1,5 @@
 from talon import Context, actions
+
 ctx = Context()
 
 ctx.matches = r"""
@@ -8,16 +9,21 @@ code.language: stata
 win.title: /^Do-file Editor/
 """
 
+
 @ctx.action_class("user")
 class UserActions:
     def code_run_selection():
         actions.key("ctrl-d")
 
     def code_run_line_start():
-        print("This action cannot be implemented in stata's do-file editor because it automatically selects the whole line.")
+        print(
+            "This action cannot be implemented in stata's do-file editor because it automatically selects the whole line."
+        )
 
     def code_run_line_end():
-        print("This action cannot be implemented in stata's do-file editor because it automatically selects the whole line.")
+        print(
+            "This action cannot be implemented in stata's do-file editor because it automatically selects the whole line."
+        )
 
     def code_run_line():
         actions.edit.select_line()
